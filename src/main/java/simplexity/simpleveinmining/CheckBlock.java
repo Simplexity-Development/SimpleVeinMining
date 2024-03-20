178622f2bbe798f0493253e4ff290b92c522b874
Jared Seville <Peashooter101@yahoo.com>
2024-03-19 17:00:55 -0700

    DFS to BFS
    Modify algorithm used from a DFS-like algorithm to a BFS-like one.
    
    Blocks are now obtained closest to the block initially broken.

--- src/main/java/simplexity/simpleveinmining/CheckBlock.java
+++ src/main/java/simplexity/simpleveinmining/CheckBlock.java
@@ -5,6 +5,8 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 import java.util.function.Consumer;
 
@@ -18,23 +20,28 @@ public class CheckBlock {
     
     public Set<Location> getBlockList(Set<Material> targets, Location start, int max) {
         Set<Location> valid_blocks = new HashSet<>();
-        checkBlockRecursive(targets, valid_blocks, start, max);
+        Queue<Location> next_blocks = new LinkedList<>();
+        checkBlockRecursive(targets, valid_blocks, next_blocks, start, max);
         return valid_blocks;
     }
     
-    public void checkBlockRecursive(Set<Material> targets, Set<Location> valid, Location current, int max) {
+    public void checkBlockRecursive(Set<Material> targets, Set<Location> valid, Queue<Location> next, Location current, int max) {
         valid.add(current);
         int[] positions = {-1, 0, 1};
         for (int x : positions) {
             for (int y : positions) {
                 for (int z : positions) {
                     if (x == 0 && y == 0 && z == 0) continue;
-                    Location next = current.clone().add(x, y, z);
-                    if (valid.size() < max && !valid.contains(next) && targets.contains(next.getBlock().getType())) {
-                        checkBlockRecursive(targets, valid, next, max);
+                    Location nextLocation = current.clone().add(x, y, z);
+                    if (valid.size() < max && !valid.contains(nextLocation) && targets.contains(nextLocation.getBlock().getType())) {
+                        next.add(nextLocation);
+                        valid.add(nextLocation);
                     }
                 }
             }
+        }
+        while (!next.isEmpty() && valid.size() < max) {
+            checkBlockRecursive(targets, valid, next, next.poll(), max);
         }
     }
 }
